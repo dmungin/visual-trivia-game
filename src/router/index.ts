0b5ab23ebfb5c81c03bf1617b71ef6ceb13a1d42
David Mungin <david.mungin@gmail.com>
2025-12-23 10:56:14 -0500

    add github pages workflow

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import GameSetup from '../components/GameSetup.vue'
 import ImageCurator from '../components/ImageCurator.vue'
 import RoundAssignment from '../components/RoundAssignment.vue'
 import GameBoard from '../components/GameBoard.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
